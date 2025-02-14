1c0e95b64c484ce73d564e827b96954218099e00
kingyj00 <onshopyj@gmail.com>
2025-02-14 10:48:06 +0900

    Practice-15-문자열 템플릿 (String Template)

---
+++ src/main/kotlin/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Alice";
+        int age = 25;
+
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+}
