278f5f778d571d221f84beefcf6879a842f4703b
kingyj00 <onshopyj@gmail.com>
2025-02-14 11:21:15 +0900

    Practice-17-JAVA:when / Kotlin:Switch

--- src/main/kotlin/Main.java
+++ src/main/kotlin/Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            int result = 10 / 0; // 0으로 나누는 예외 발생
-            System.out.println("결과: " + result);
-        } catch (ArithmeticException e) { // 예외 처리
-            System.out.println("예외 발생: " + e.getMessage());
-        } finally {
-            System.out.println("이 코드는 항상 실행됩니다.");
+        int day = 3;
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            default:
+                dayName = "Invalid Day";
         }
+
+        System.out.println("Day: " + dayName);
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
 package com.ll
 
 fun main() {
-    try {
-        val result = 10 / 0 // 0으로 나누는 예외 발생
-        println("결과: $result")
-    } catch (e: ArithmeticException) { // 예외 처리
-        println("예외 발생: " + e.message)
-    } finally {
-        println("이 코드는 항상 실행됩니다.")
+    val day = 3
+
+    val dayName = when (day) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        else -> "Invalid Day"
     }
+
+    println("Day: $dayName")
 }
